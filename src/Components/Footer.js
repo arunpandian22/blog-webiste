1876bded48e5b4480350bb2748047b36d135eea9
Arun Pandian <arunpandian@fundsindia.com>
2023-12-03 20:28:25 +0530

    Changed: Footer and header changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,7 @@ export default () => {
     <footer>
       <div className="footer">
         <ul>
-          <li>React JS</li>
-          <li>React Router</li>
-          <li>Draft JS</li>
-          <li>Love</li>
-          <li>By <a href="https://www.zonayed.me" rel="noopener noreferrer" target="_blank">Zonayed Ahmed</a></li>
+          <li>Love <span role='img' aria-label="heart">❤️</span> By Arun Pandian Dharanya</li>
         </ul>
       </div>
     </footer>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ export default ({match: {url}}) => {
       <div className="navbar">
         <ul>
           <li>{url === '/' ? <Link to="/new" className="btn btn-new">New Story</Link> : <Link to="/" className="btn btn-new">Home</Link>}</li>
-          <li><img alt="react-logo" src={logo} /></li>
+          <li className='btn btn-bookmarks'>About</li>
           <li><Link to="/bookmark" className="btn btn-bookmarks">Bookmarks</Link></li>
         </ul>
       </div>
